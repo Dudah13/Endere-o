6bfb618471dd1e1c4524b81071af9e331e0385d3
Administrador <Administrador@10.10.4.28>
2019-08-01 19:20:49 -0300

    ta bagual

---
+++ Endereco/src/Dao/EnderecoDao.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Endereco;
+/**
+ *
+ * @author Administrador
+ */
+public class EnderecoDao {
+    public static boolean inserir(Endereco objeto) {
+        String sql = "INSERT INTO endereco (logradouro, complemento, bairro) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getLogradouro());
+            ps.setString(2, objeto.getComplemento());
+            ps.setString(3, objeto.getBairro());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Endereco objeto = new Endereco();
+        objeto.setLogradouro("BR");
+        objeto.setComplemento("Brasil");
+        objeto.setBairro("Brasil");
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+    
+    public static boolean alterar(Endereco objeto) {
+        String sql = "UPDATE endereco SET bairro = centro, complemento = 342 WHERE codigo=1";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getBairro()); 
+            ps.setString(2, objeto.getComplemento());
+            ps.setInt(3, objeto.getCodigo());
+            ps.setString(4, objeto.getLogradouro());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+}
